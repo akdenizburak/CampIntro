ddddba6c0c07a700a255a7b7e0bca2a9a6e4efcc
Burak Akdeniz <akdenizburakk7@gmail.com>
2021-02-16 05:49:38 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product { Id = 1, CategoryId = 2, ProductName = "Masa", UnitPrice = 500, UnitsInStock = 3 };
+            Product product2 = new Product { Id = 2, CategoryId = 3, ProductName = "Kalem", UnitPrice = 78, UnitsInStock = 200 };
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+
+        }
+    }
+}
